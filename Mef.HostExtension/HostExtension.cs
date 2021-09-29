cdfde76adb7e36983d0c98bad8d9019678f26cbe
Hank McCord <hymccord@gmail.com>
2021-09-29 14:12:07 -0400

    Add internal (host) extension example

---
+++ Mef.HostExtension/HostExtension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.IO;
+using System.Reflection;
+
+using Mef.Contracts;
+
+using Newtonsoft.Json;
+
+namespace Mef.HostExtension
+{
+    [Export(typeof(IExtension))]
+    public class HostExtension : IExtension
+    {
+        private readonly IImport _import;
+
+        [ImportingConstructor]
+        public HostExtension(IImport import)
+        {
+            _import = import;
+
+            var assembly = Assembly.GetExecutingAssembly();
+            var jsonAssembly = typeof(JsonConvert).Assembly;
+            Console.WriteLine($@"   {nameof(HostExtension)} created
+        My directory is: {Directory.GetParent(assembly.Location).FullName}
+        Using Newtonsoft.Json {jsonAssembly.FullName}
+        {JsonConvert.ToString(DateTime.Now)}");
+
+        }
+    }
+}
