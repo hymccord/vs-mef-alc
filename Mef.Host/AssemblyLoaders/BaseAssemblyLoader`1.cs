0ea1b5ac92179cb86e991fdd1b745522dda27ccd
Hank McCord <hymccord@gmail.com>
2021-09-29 14:20:10 -0400

    Add infra for custom ALC resolvers

---
+++ Mef.Host/AssemblyLoaders/BaseAssemblyLoader`1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.Loader;
+
+using Microsoft.VisualStudio.Composition;
+
+namespace Mef.Host
+{
+    public abstract class BaseAssemblyLoader<T> : IAssemblyLoader where T : AssemblyLoadContext
+    {
+        /// <summary>
+        /// A cache of assembly names to loaded assemblies.
+        /// </summary>
+        private readonly Dictionary<AssemblyName, Assembly> _loadedAssemblies = new Dictionary<AssemblyName, Assembly>(ByValueEquality.AssemblyName);
+        private readonly ConcurrentDictionary<AssemblyName, AssemblyLoadContext> _loadedContexts = new ConcurrentDictionary<AssemblyName, AssemblyLoadContext>(ByValueEquality.AssemblyName);
+
+        /// <inheritdoc />
+        public Assembly LoadAssembly(AssemblyName assemblyName)
+        {
+            Assembly? assembly;
+            lock (_loadedAssemblies)
+            {
+                _loadedAssemblies.TryGetValue(assemblyName, out assembly);
+            }
+
+            if (assemblyName.CodeBase is null)
+            {
+                throw new ArgumentException("Codebase cannot be null", nameof(assemblyName));
+            }
+
+            if (assembly == null)
+            {
+                AssemblyLoadContext loadContext;
+                lock (_loadedContexts)
+                {
+                    loadContext = _loadedContexts.GetOrAdd(assemblyName, (an) =>
+                    {
+                        return (AssemblyLoadContext)Activator.CreateInstance(typeof(T), assemblyName.CodeBase)!;
+                    });
+                }
+
+                assembly = loadContext.LoadFromAssemblyPath(assemblyName.CodeBase);
+
+
+                lock (_loadedAssemblies)
+                {
+                    _loadedAssemblies[assemblyName] = assembly;
+                }
+            }
+
+            return assembly;
+        }
+
+        /// <inheritdoc />
+        /// <remarks>This one is never used by VS-MEF currently.</remarks>
+        public Assembly LoadAssembly(string assemblyFullName, string? codeBasePath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Mef.Host/Resolvers/CustomResolver`1.cs
@@ -0,0 +1,10 @@
+﻿
+using Microsoft.VisualStudio.Composition;
+
+namespace Mef.Host
+{
+    public class CustomResolver<T> : Resolver where T : IAssemblyLoader, new()
+    {
+        public CustomResolver() : base(new T()) { }
+    }
+}
